87966064e1b74fa1fbd5ea8e6eb9d43768f1389d
Frederik Thomsen <>
2019-05-30 10:46:14 +0200

    Fixed issue when not giving any column to order by

--- src/Macros/Order.php
+++ src/Macros/Order.php
@@ -6,7 +6,7 @@ use Vestervang\AgileResource\Models\BaseModel;
 
 Builder::macro('order', function () {
 
-    if(!$this->model instanceof BaseModel) {
+    if (!$this->model instanceof BaseModel) {
         throw new Exception('Model should of type: ' . BaseModel::class);
     }
 
@@ -22,24 +22,25 @@ Builder::macro('order', function () {
         $orderDir = (isset($orderDirs[$i]) && $orderDirs[$i] !== '')
             ? strtolower($orderDirs[$i]) : $this->model->getOrderDirection();
 
-        if ($orderBy === null) {
-            return $this;
-        }
 
         $key = array_search($orderBy, array_column($this->model->getMapping(), 'frontend'));
 
-        if($key === false) {
+        if ($key === false && $orderBy !== null) {
             throw new Exception("{$orderBy} is not a valid attribute of " . get_class($this->model));
         }
 
         $modelColumn = $this->model->getMapping()[$key]['backend'];
 
-        if (!in_array($modelColumn, $this->model->getSortableColumns())) {
-            throw new Exception("column {$orderBy} is not sortable",HttpCode::BAD_REQUEST);
+        if ($orderBy !== null && !in_array($modelColumn, $this->model->getSortableColumns())) {
+            throw new Exception("column {$orderBy} is not sortable", HttpCode::BAD_REQUEST);
         }
 
         if (!in_array($orderDir, $orderDirectionsPossible)) {
             throw new Exception('Order direction is invalid', HttpCode::BAD_REQUEST);
+        }
+
+        if ($orderBy === null) {
+            $modelColumn = 'id';
         }
 
         $this->orderBy($modelColumn, $orderDir);
