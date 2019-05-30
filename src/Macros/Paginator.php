8babd52f0db7a8498c4e2e981d29443d4bf137b1
Frederik Thomsen <>
2019-05-30 10:36:16 +0200

    Added exception when model isn't extending the BaseModel

--- src/Macros/Paginator.php
+++ src/Macros/Paginator.php
@@ -3,8 +3,15 @@
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pagination\Paginator;
+use Vestervang\AgileResource\Models\BaseModel;
+
 
 Builder::macro('paginator', function ($columns = ['*'], $pageName = 'page', $page = null) {
+
+    if(!$this->model instanceof BaseModel) {
+        throw new Exception('Model has to be a instance of ' . BaseModel::class);
+    }
+
     $page = $page ?: Paginator::resolveCurrentPage($pageName);
 
     // Default to 15 if nothing is returned
